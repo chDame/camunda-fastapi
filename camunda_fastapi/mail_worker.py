a1618a8f7bba017273df9179e0904b5bb3d36cef
Christophe <christophe.dame@camunda.com>
2024-01-09 11:43:10 +0100

    show an example of business error

--- camunda_fastapi/mail_worker.py
+++ camunda_fastapi/mail_worker.py
@@ -1,11 +1,15 @@
 import base64
 
 from pyzeebe import Job
+from pyzeebe.errors import BusinessError
 from camunda_fastapi.services import mail_service
 
 def create_mail_task(worker):
     @worker.task(task_type="email", single_value=False)
     async def mail(*args, **kwargs) -> dict:
         print(type(kwargs))
-        mail_service.send_mail(kwargs["to"], kwargs["subject"], kwargs["template"], kwargs["locale"], kwargs)
+        if kwargs["to"]=="christophe.dame@camunda.com":
+            raise BusinessError("MAIL_FORMAT_ERROR")
+        else:
+            mail_service.send_mail(kwargs["to"], kwargs["subject"], kwargs["template"], kwargs["locale"], kwargs)
         return {}
